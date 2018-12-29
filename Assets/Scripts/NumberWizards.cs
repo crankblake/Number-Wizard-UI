56c904914e1cc681a54b4379d44897faf484c7a0
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2018-12-28 22:27:14 -0700

    Scenes and GUI stuff and refactor

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+	// Use this for initialization
+	[SerializeField] int max;
+    [SerializeField] int min;
+	int guess;
+	
+	void Start () {
+		StartGame();
+	}
+	void StartGame () {
+		max = max + 1;
+        guess = (max + min) / 2;
+    }
+	public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+	void NextGuess () {
+		guess = (max + min) / 2;
+	}
+}
